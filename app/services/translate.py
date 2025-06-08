e6b075e1ebb68ae121cec91d33ece135badeed51
endermaru <endermaru>
2025-06-08 18:07:01 +0900

    Initial commit

---
+++ app/services/translate.py
@@ -0,0 +1,20 @@
+import requests
+from app.config import settings
+
+def deepl_translate(command, source="KO", target="EN", auth_key=settings.api_key):
+    url = "https://api-free.deepl.com/v2/translate"
+    data = {
+        "auth_key": auth_key,
+        "text": command,
+        "source_lang": source,
+        "target_lang": target,
+    }
+
+    try:
+        response = requests.post(url, data=data, timeout=10)
+        if response.status_code == 200:
+            return response.json()['translations'][0]['text']
+        else:
+            raise Exception(f"Error: {response.status_code}, {response.text}")
+    except Exception as e:
+        return command  # 실패 시 원문 유지
